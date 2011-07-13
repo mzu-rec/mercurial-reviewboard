979359fe911227b97897208c73744aa8d564c646
Dan Helfman <dan@energysavvy.com>
2011-07-13 11:44:28 -0700

    Potential fix for https://bitbucket.org/mdelagra/mercurial-reviewboard/issue/18/

---
+++ mercurial_reviewboard/tests/test_commands.py
@@ -0,0 +1,32 @@
+from nose.tools import eq_
+
+from mercurial import fancyopts
+from mercurial_reviewboard import cmdtable
+
+def test_existing():
+    opts = {}
+    fancyopts.fancyopts(['-e', '101'], cmdtable['postreview'][1], opts, True)
+    eq_('101', opts['existing'])
+    
+def test_update():
+    opts = {}
+    args = fancyopts.fancyopts(['-u'], cmdtable['postreview'][1], opts, True)
+    eq_(True, opts['update'])    
+    
+def test_target_groups():
+    opts = {}
+    args = fancyopts.fancyopts(['-G', 'foo, bar'], cmdtable['postreview'][1], 
+                               opts, True)
+    eq_('foo, bar', opts['target_groups'])
+
+def test_target_people():
+    opts = {}
+    args = fancyopts.fancyopts(['-U', 'john, jane'], cmdtable['postreview'][1], 
+                               opts, True)
+    eq_('john, jane', opts['target_people'])
+    
+def test_repoid():
+    opts = {}
+    args = fancyopts.fancyopts(['-i', '101'], cmdtable['postreview'][1], 
+                               opts, True)
+    eq_('101', opts['repoid'])
---
+++ mercurial_reviewboard/tests/test_outgoing.py
@@ -0,0 +1,103 @@
+from mock import patch
+from nose.tools import eq_
+
+from mercurial import util
+from mercurial_reviewboard import postreview
+from mercurial_reviewboard.tests import get_initial_opts, get_repo, mock_ui
+
+
+@patch('mercurial_reviewboard.send_review')
+def test_outgoing(mock_send):
+    ui = mock_ui()
+    repo = get_repo(ui, 'two_revs')
+    opts = get_initial_opts()
+    opts['outgoingrepo'] = 'mercurial_reviewboard/tests/repos/no_revs'
+    opts['outgoingchanges'] = True
+    postreview(ui, repo, **opts)
+    
+    expected = open('mercurial_reviewboard/tests/diffs/outgoing', 'r').read()
+    eq_(expected, mock_send.call_args[0][4])
+
+
+@patch('mercurial_reviewboard.send_review')
+def test_outgoing_one_rev(mock_send):
+    ui = mock_ui()
+    repo = get_repo(ui, 'one_rev')
+    opts = get_initial_opts()
+    opts['outgoingrepo'] = 'mercurial_reviewboard/tests/repos/no_revs'
+    opts['outgoingchanges'] = True
+    postreview(ui, repo, **opts)
+    
+    expected = open('mercurial_reviewboard/tests/diffs/outgoing_one_rev', 
+        'r').read()
+    eq_(expected, mock_send.call_args[0][4])
+
+
+@patch('mercurial_reviewboard.send_review')
+def test_outgoing_one_rev_no_outgoing_flag(mock_send):
+    try:
+        ui = mock_ui()
+        repo = get_repo(ui, 'one_rev')
+        opts = get_initial_opts()
+        opts['outgoingchanges'] = True
+        postreview(ui, repo, **opts)
+    
+        expected = open('mercurial_reviewboard/tests/diffs/outgoing_one_rev', 
+            'r').read()
+        eq_(expected, mock_send.call_args[0][4])
+    except util.Abort, e:
+        expected = ("When using the -g/--outgoingchanges flag, you must "
+            "also use either the -o or the -O <repo> flag.")
+        eq_(expected, e.__str__())
+
+
+@patch('mercurial_reviewboard.send_review')
+def test_outgoing_with_branch(mock_send):
+    '''Test that only one change is included, despite a commit to another 
+    branch.'''
+    ui = mock_ui()
+    repo = get_repo(ui, 'two_revs_clone')
+    opts = get_initial_opts()
+    opts['outgoingrepo'] = 'mercurial_reviewboard/tests/repos/two_revs'
+    opts['outgoingchanges'] = True
+    postreview(ui, repo, '2', **opts)
+    
+    expected = open('mercurial_reviewboard/tests/diffs/outgoing_with_branch', 
+                    'r').read()
+    eq_(expected, mock_send.call_args[0][4])
+
+
+@patch('mercurial_reviewboard.send_review')
+def test_no_outgoing_no_revs(mock_send):
+    try:
+        ui = mock_ui()
+        repo = get_repo(ui, 'no_revs')
+        opts = get_initial_opts()
+        opts['outgoingrepo'] = 'mercurial_reviewboard/tests/repos/no_revs'
+        opts['outgoingchanges'] = True
+        postreview(ui, repo, **opts)
+        assert 0, "Should have raised an Abort."
+    except util.Abort, e:
+        check_parent_rev_exception(e)
+
+
+@patch('mercurial_reviewboard.send_review')
+def test_no_outgoing_two_revs(mock_send):
+    try:
+        ui = mock_ui()
+        repo = get_repo(ui, 'two_revs')
+        opts = get_initial_opts()
+        opts['outgoingrepo'] = 'mercurial_reviewboard/tests/repos/two_revs'
+        opts['outgoingchanges'] = True
+        postreview(ui, repo, **opts)
+        assert 0, "Should have raised an Abort."
+    except util.Abort, e:
+        check_parent_rev_exception(e)
+
+
+def check_parent_rev_exception(e):
+    eq_("Unable to determine parent revision for diff. "
+        "If using -g/--outgoingchanges, make sure you have some "
+        "(type 'hg out'). Did you forget to commit ('hg st')?",
+        e.__str__()
+    )
